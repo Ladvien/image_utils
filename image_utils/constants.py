44433c647e30fe36f5889d6d521514aa095e1224
C. Thomas Brittain <cthomasbrittain@hotmail.com>
2025-05-01 07:24:49 -0500

    Init

---
+++ image_utils/constants.py
@@ -0,0 +1,13 @@
+IMAGE_TYPES_LOWERCASE = [
+    "jpg",
+    "jpeg",
+    "png",
+    "gif",
+    "webp",
+    "tiff",
+    "bmp",
+    "svg",
+    "ico",
+]
+
+IMAGE_TYPES_UPPERCASE = [image_type.upper() for image_type in IMAGE_TYPES_LOWERCASE]
